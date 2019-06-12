bca23594a1c2496e163ce9cafea2261a94062e81
Avinashdante <avinash.nani789@gmail.com>
2019-06-13 02:48:17 +0530

    Jaffa Method

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  res :any
+  display = ""
+  title = 'calculator';
+  flag = 0
+  insertChar(num){
+   this.display = this.display + num;
+  }
+  evaluate(){
+    console.log();
+    this.res = eval(this.display)
+  }
+  reset(){
+    this.res = undefined
+    this.display = ""
+  }
+
+}
